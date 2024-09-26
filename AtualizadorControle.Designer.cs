6c2a9ec08f547f4f914ba333c2b130eec07eb435
RafaelMeritello <121911971+Rafaelmeritello@users.noreply.github.com>
2024-09-26 16:33:28 -0300

    Add files via upload

---
+++ AtualizadorControle.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace FerramentasUC4X
+{
+    partial class AtualizadorControle
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AtualizadorControle));
+            this.grid_nao_iniciados = new System.Windows.Forms.DataGridView();
+            this.Telefone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_imp_controle = new System.Windows.Forms.Button();
+            this.btn_imp_vethor = new System.Windows.Forms.Button();
+            this.btn_atualizar = new System.Windows.Forms.Button();
+            this.labelvethor = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.vethor_status_label = new System.Windows.Forms.Label();
+            this.controle_status_label = new System.Windows.Forms.Label();
+            this.barra_progresso = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.grid_nao_iniciados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grid_nao_iniciados
+            // 
+            this.grid_nao_iniciados.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.grid_nao_iniciados.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.grid_nao_iniciados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid_nao_iniciados.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Telefone});
+            this.grid_nao_iniciados.Location = new System.Drawing.Point(13, 47);
+            this.grid_nao_iniciados.Name = "grid_nao_iniciados";
+            this.grid_nao_iniciados.Size = new System.Drawing.Size(554, 195);
+            this.grid_nao_iniciados.TabIndex = 0;
+            // 
+            // Telefone
+            // 
+            this.Telefone.HeaderText = "ID";
+            this.Telefone.Name = "Telefone";
+            this.Telefone.ReadOnly = true;
+            // 
+            // btn_imp_controle
+            // 
+            this.btn_imp_controle.Location = new System.Drawing.Point(13, 310);
+            this.btn_imp_controle.Name = "btn_imp_controle";
+            this.btn_imp_controle.Size = new System.Drawing.Size(283, 33);
+            this.btn_imp_controle.TabIndex = 1;
+            this.btn_imp_controle.Text = "Importar planilha controle";
+            this.btn_imp_controle.UseVisualStyleBackColor = true;
+            this.btn_imp_controle.Click += new System.EventHandler(this.btn_imp_controle_Click);
+            // 
+            // btn_imp_vethor
+            // 
+            this.btn_imp_vethor.Location = new System.Drawing.Point(13, 255);
+            this.btn_imp_vethor.Name = "btn_imp_vethor";
+            this.btn_imp_vethor.Size = new System.Drawing.Size(283, 49);
+            this.btn_imp_vethor.TabIndex = 3;
+            this.btn_imp_vethor.Text = "Importar Vethor";
+            this.btn_imp_vethor.UseVisualStyleBackColor = true;
+            this.btn_imp_vethor.Click += new System.EventHandler(this.btn_imp_vethor_Click);
+            // 
+            // btn_atualizar
+            // 
+            this.btn_atualizar.Location = new System.Drawing.Point(471, 255);
+            this.btn_atualizar.Name = "btn_atualizar";
+            this.btn_atualizar.Size = new System.Drawing.Size(95, 88);
+            this.btn_atualizar.TabIndex = 4;
+            this.btn_atualizar.Text = "Atualizar";
+            this.btn_atualizar.UseVisualStyleBackColor = true;
+            this.btn_atualizar.Click += new System.EventHandler(this.btn_atualizar_Click);
+            // 
+            // labelvethor
+            // 
+            this.labelvethor.AutoSize = true;
+            this.labelvethor.Location = new System.Drawing.Point(302, 273);
+            this.labelvethor.Name = "labelvethor";
+            this.labelvethor.Size = new System.Drawing.Size(83, 13);
+            this.labelvethor.TabIndex = 5;
+            this.labelvethor.Text = "Planilha vethor: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(302, 320);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Planilha controle: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.25F);
+            this.label4.Location = new System.Drawing.Point(9, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "ID\'s Filtrados";
+            // 
+            // vethor_status_label
+            // 
+            this.vethor_status_label.AutoSize = true;
+            this.vethor_status_label.ForeColor = System.Drawing.Color.Red;
+            this.vethor_status_label.Location = new System.Drawing.Point(398, 273);
+            this.vethor_status_label.Name = "vethor_status_label";
+            this.vethor_status_label.Size = new System.Drawing.Size(53, 13);
+            this.vethor_status_label.TabIndex = 9;
+            this.vethor_status_label.Text = "Pendente";
+            // 
+            // controle_status_label
+            // 
+            this.controle_status_label.AutoSize = true;
+            this.controle_status_label.ForeColor = System.Drawing.Color.Red;
+            this.controle_status_label.Location = new System.Drawing.Point(398, 320);
+            this.controle_status_label.Name = "controle_status_label";
+            this.controle_status_label.Size = new System.Drawing.Size(53, 13);
+            this.controle_status_label.TabIndex = 10;
+            this.controle_status_label.Text = "Pendente";
+            // 
+            // barra_progresso
+            // 
+            this.barra_progresso.Location = new System.Drawing.Point(13, 5);
+            this.barra_progresso.Name = "barra_progresso";
+            this.barra_progresso.Size = new System.Drawing.Size(553, 23);
+            this.barra_progresso.TabIndex = 12;
+            // 
+            // AtualizadorControle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 355);
+            this.Controls.Add(this.barra_progresso);
+            this.Controls.Add(this.controle_status_label);
+            this.Controls.Add(this.vethor_status_label);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelvethor);
+            this.Controls.Add(this.btn_atualizar);
+            this.Controls.Add(this.btn_imp_vethor);
+            this.Controls.Add(this.btn_imp_controle);
+            this.Controls.Add(this.grid_nao_iniciados);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "AtualizadorControle";
+            this.Text = "Atualizador Controle de expedição";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.AtualizadorControle_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.grid_nao_iniciados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grid_nao_iniciados;
+        private System.Windows.Forms.Button btn_imp_controle;
+        private System.Windows.Forms.Button btn_imp_vethor;
+        private System.Windows.Forms.Button btn_atualizar;
+        private System.Windows.Forms.Label labelvethor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label vethor_status_label;
+        private System.Windows.Forms.Label controle_status_label;
+        private System.Windows.Forms.ProgressBar barra_progresso;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telefone;
+    }
+}
+
---
+++ AtualizadorUpload.cs
@@ -0,0 +1,224 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FerramentasUC4X.modulos
+{
+    public partial class AtualizadorUpload : Form
+    {
+        public AtualizadorUpload()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_imp_controle_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialogoabrir = new OpenFileDialog();
+
+            dialogoabrir.Filter = "Excel Files|*.xls;*.xlsx";
+            dialogoabrir.Title = "Selecione um arquivo Excel (Planilha controle)";
+            if(dialogoabrir.ShowDialog() == DialogResult.OK)
+            {
+                VariaveisGlobais.caminhocontroleatualizado = dialogoabrir.FileName;
+                MessageBox.Show("Controle importado", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                statuscontrole.Text = Path.GetFileName( dialogoabrir.FileName);
+                statuscontrole.ForeColor = Color.Green;
+            }
+        }
+
+        private void btn_importar_upload_Click(object sender, EventArgs e)
+        {
+            {
+                OpenFileDialog dialogoabrir = new OpenFileDialog();
+
+                dialogoabrir.Filter = "Excel Files|*.xls;*.xlsx";
+                dialogoabrir.Title = "Selecione um arquivo Excel (Planilha upload)";
+                if (dialogoabrir.ShowDialog() == DialogResult.OK)
+                {
+                    VariaveisGlobais.caminhoplanilhaupload = dialogoabrir.FileName;
+                    MessageBox.Show("Upload importado", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information) ;
+                    statusupload.Text = Path.GetFileName(dialogoabrir.FileName);
+                    statusupload.ForeColor = Color.Green;
+                }
+            }
+
+        }
+
+        private async void btn_consolidar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                await Consolidar();
+            }
+            catch
+            {
+                MessageBox.Show("Erro ao consolidar, verifique se alguma planilha ja esta aberta em outro programa", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+
+
+        
+
+        }
+        public async Task Consolidar()
+        {
+            Invoke((Action)(() =>
+            {
+                barra_progresso.Maximum = 100;
+                barra_progresso.Value = 0;
+                barra_progresso.Style = ProgressBarStyle.Marquee; 
+            }));
+
+            await Task.Run(() =>
+            {
+                using (var workbookControle = new XLWorkbook(VariaveisGlobais.caminhocontroleatualizado))
+                {
+                    
+                    Invoke((Action)(() =>
+                    {
+                        barra_progresso.Style = ProgressBarStyle.Blocks; 
+                        barra_progresso.Value = 0; 
+                    }));
+
+                    var sheetcontrole = workbookControle.Worksheet("fluxo");
+                    var tabelacontrole = sheetcontrole.Table("tabela1");
+                    var cabecalhocontrole = tabelacontrole.Row(1);
+                    var colunaconsolidadoid = cabecalhocontrole.Cells()
+                        .FirstOrDefault(c => c.Value.ToString() == "CONSOLIDADO").Address.ColumnNumber;
+
+                    using (var workbookUpload = new XLWorkbook(VariaveisGlobais.caminhoplanilhaupload))
+                    {
+                        var sheetupload = workbookUpload.Worksheet(1);
+                        var cabecalhoupload = sheetupload.Row(1);
+
+                        // Criar um dicionário para mapear os índices das colunas da planilha de upload
+                        var indicesColunasUpload = new Dictionary<string, List<int>>();
+
+                        foreach (var cell in cabecalhoupload.Cells())
+                        {
+                            var nomeCabecalho = cell.GetString();
+                            var index = cell.Address.ColumnNumber;
+
+                            if (!indicesColunasUpload.ContainsKey(nomeCabecalho))
+                            {
+                                indicesColunasUpload[nomeCabecalho] = new List<int>();
+                            }
+                            indicesColunasUpload[nomeCabecalho].Add(index);
+                        }
+
+                
+                        var dadosParaUpload = new List<List<object>>();
+
+                        // Contar o número total de linhas a serem processadas
+                        var totalLinhas = tabelacontrole.DataRange.RowCount() - 1; // -1 para ignorar o cabeçalho
+                        int linhaAtual = 0;
+
+                        // Iterar sobre as linhas da tabela, começando da linha 2
+                        foreach (var linha in tabelacontrole.DataRange.Rows().Skip(1))
+                        {
+                            linhaAtual++;
+                      
+                            // Verificar se o valor na coluna "CONSOLIDADO" é "NÃO"
+                            if (linha.Cell(colunaconsolidadoid).Value.ToString().Equals("NÃO", StringComparison.OrdinalIgnoreCase))
+                            {
+                                // Adicionar os dados da linha na lista
+                                linha.Cell(colunaconsolidadoid).SetValue("SIM");
+                                var linhaDados = linha.Cells().Select(c => (object)c.Value).ToList();
+                                dadosParaUpload.Add(linhaDados);
+                            }
+
+                          
+                            var progresso = (int)((linhaAtual / (float)totalLinhas) * 100);
+                            Invoke((Action)(() => barra_progresso.Value = progresso));
+                        }
+
+                       
+                        int linhaUpload = 2;
+                        foreach (var dados in dadosParaUpload)
+                        {
+                            for (int i = 0; i < dados.Count; i++)
+                            {
+                               
+                                var nomeCabecalho = cabecalhocontrole.Cell(i + 1).GetString();
+                                if (indicesColunasUpload.TryGetValue(nomeCabecalho.Trim(), out var colunasUpload))
+                                {
+                        
+                                    foreach (var colunaUpload in colunasUpload)
+                                    {
+                                        
+                                            
+                                            if(sheetupload.Row(1).Cell(colunaUpload).Value.ToString().Trim() == "ID")
+                                            {
+                                              
+                                               
+                                            Invoke((Action)(() => IdsNecessarios.Rows.Add(dados[i].ToString())));
+                                        }
+                                          
+                                            if (dados[i] is string)
+                                            {
+                                                sheetupload.Cell(linhaUpload, colunaUpload).SetValue((string)dados[i]);
+                                            }
+                                            else if (dados[i] is int)
+                                            {
+                                                sheetupload.Cell(linhaUpload, colunaUpload).SetValue((int)dados[i]);
+                                            }
+                                            else if (dados[i] is double)
+                                            {
+                                                sheetupload.Cell(linhaUpload, colunaUpload).SetValue((double)dados[i]);
+                                            }
+                                            else if (dados[i] is DateTime)
+                                            {
+                                                sheetupload.Cell(linhaUpload, colunaUpload).SetValue((DateTime)dados[i]);
+                                            }
+                                            else if (dados[i] is long)
+                                            {
+                                                sheetupload.Cell(linhaUpload, colunaUpload).SetValue((long)dados[i]);
+                                            }
+                                        else
+                                            {
+                                                sheetupload.Cell(linhaUpload, colunaUpload).SetValue(dados[i]?.ToString());
+                                            }
+                                        }
+                                    
+                                    
+                                }
+                                else
+                                {
+                    
+                                }
+                            }
+
+                            // Preencher colunas padrão
+                            sheetupload.Cell(linhaUpload, indicesColunasUpload["Integrador"][0]).SetValue("METODO");
+                            sheetupload.Cell(linhaUpload, indicesColunasUpload["TRATAMENTO | ID Identificação Parceiro"][0]).SetValue("METODO");
+                            sheetupload.Cell(linhaUpload, indicesColunasUpload["ABERTURA | Avançar para próxima etapa?"][0]).SetValue("SIM");
+
+                            linhaUpload++;
+          
+                            var progressoPreenchimento = (int)((linhaUpload - 2) / (float)dadosParaUpload.Count * 100);
+                            Invoke((Action)(() => barra_progresso.Value = progressoPreenchimento));
+                        }
+                        workbookUpload.Save();
+                        workbookControle.Save();
+                        
+                        Invoke((Action)(() => barra_progresso.Value = 0)); 
+                    }
+                }
+            });
+
+
+            MessageBox.Show("Consolidação concluída com sucesso!", "Concluído", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+
+    }
+}
